3e70a550bc2ca13543d5fd45f43bf127db5f8e92
Miquéias Ferreira Dos Santos <queias2909@gmail.com>
2026-03-10 20:09:09 -0300

    reset project

--- test/dataBaseTest.py
+++
@@ -1,7 +0,0 @@
-from Application.model.entities.Classroom import Classroom
-from Application.model.entities.DayWeek import DayWeek
-from Application.model.service.ClassroomService import ClassroomService
-from Application.model.repository.ManagerDataBase import ManagerDataBase
-
-print(ClassroomService.getByName("sala2"))
-
--- test/dateTimeTest.py
+++
@@ -1,6 +0,0 @@
-import calendar
-import datetime
-
-calendario = calendar.Calendar()
-
-mes = calendario.monthdatescalendar(2025, 8)
